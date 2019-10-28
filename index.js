e4bf5bc7d0d03f4e030f5bb928757237fe4bd9eb
Outhai SAIOUDOM <os55555gster@gmail.com>
2019-10-29 00:22:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('*', (req, res) => {
-	res.write('<h1><marquee direction=right>Hello from Express path `/` on Now 2.0!</marquee></h1>');
+	res.write('<h1><marquee direction=right>Hello World from Express path `/` on Now 2.0!</marquee></h1>');
 	res.write('<h2>Go to <a href="/about">/about</a></h2>');
 	res.end();
 });
 
-app.listen(3000, 'localhost', err => {
+app.listen(5555, 'localhost', err => {
 	if (err) {
 		console.log(err);
 	}
